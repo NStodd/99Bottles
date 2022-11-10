5915491cfa5b8edd95a192671ff3c773652582b5
Nate Stodd <nathan.stoddard@gmail.com>
2022-11-10 07:56:27 -0500

    99 little bugs functionality

--- server.js
+++ server.js
@@ -6,22 +6,32 @@ app.get('/', (req, res) => {
     res.send(`
          99 bottles of Tree House DIPA on the wall
         </br>
-        <a href="/98"> Take one down, pass it around... </a>
+        <a href="/98"> Take one down, </br>
+        patch it around... </a>
     `)
 })
 
 app.get('/:bottles', (req, res) => {
     const current = parseInt(req.params.bottles)
-    const next = current - 1
-    let next_button = `<a href="/${next}"> Take one down, pass it around... </a>`
+    let next = current - 1
+    
+    if (Math.random() * 100 < 5) {
+        next = next + Math.floor(Math.random() * 100)
+    }
+
+    let next_button = `
+        <a href="/${next}">
+        Take one down,</br>
+        patch it around... </a>
+    `
 
     if (current === 0) {
         next_button = '<a href="/"> Start the song again. </a>'
     }
 
     res.send(`
-        ${current} bottles of Tree House DIPA on the wall
-        </br>
+        ${current} little bugs in the code, </br>
+        ${current} little bugs... </br>
         ${next_button}
     `)
 }) // route template
